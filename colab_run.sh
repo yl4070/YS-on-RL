182911ae6fef607f9e19b635f7da086762557818
yl4070 <yl4070@rit.edu>
2026-08-09 15:58:14 -0400

    fix GPU config

--- colab_run.sh
+++ colab_run.sh
@@ -12,6 +12,10 @@ adduser --home /home/runner --gecos "" --shell /bin/bash --disabled-password run
 # pull the pytorch_for_rl image
 ./install-dir/bin/apptainer build pytorch_for_rl.sif docker://esppk/pytorch_for_rl:latest
 
+#fixing the nvidia driver issue
+echo "/usr/lib64-nvidia" >> /etc/ld.so.conf.d/nvidia.conf
+ldconfig
+
 # run script with docker images
 sudo -u runner ./install-dir/bin/apptainer exec --bind /content:/mnt/data --nv pytorch_for_rl.sif python /mnt/data/YS-on-RL/run.py
 
